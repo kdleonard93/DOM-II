0b66178f5bf6fa2362644a6908bcc6374ed50a62
Kyle Leonard <kdleonard@icloud.com>
2019-08-11 20:20:51 -0500

    Added button event listeners and 1 P tag event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+// Button variables
+const btn1 = document.querySelectorAll(".btn")[0];
+const btn2 = document.querySelectorAll(".btn")[1];
+const btn3 = document.querySelectorAll(".btn")[2];
+
+
+btn1.addEventListener("click", () => {
+btn1.style.color ="red";
+});
+
+btn2.addEventListener("mouseover", () => {
+btn2.style.backgroundColor = "goldenrod";
+});
+
+btn3.addEventListener("dblclick", () => {
+btn3.style.borderRadius = "40px";
+});
+
+
+// text variables
+
+const bodyText = document.querySelector("p");
+
+document.addEventListener("keydown", () => {
+    bodyText.style.color = "teal";
+});
+
---
+++ stretch-assignment/index.js
@@ -0,0 +1,10 @@
+const btn1 = document.querySelectorAll(".btn")[0];
+const btn2 = document.querySelectorAll(".btn")[1];
+const btn3 = document.querySelectorAll(".btn")[2];
+
+
+btn1.addEventListener("click", () => {
+btn1.style.color ="red";
+});
+
+console.log("hey");
